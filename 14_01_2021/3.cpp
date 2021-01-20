2db02f26847587b876142a9072d4a387ba3f30b0
Matej Dinis <dinismatej1@gmail.com>
2021-01-20 19:31:30 +0000

    .

--- 14_01_2021/3.cpp
+++ 14_01_2021/3.cpp
@@ -2,16 +2,16 @@
 #include <string>
 
 auto main(int argc, const char** argv) -> int {
-    std::string a, b, c, d;
+    std::string a, b;
     std::cout<<"Zadaj 1. retazec: ";
     std::getline(std::cin, a);
+    b.append(a);
     std::cout<<"Zadaj 2. retazec: ";
-    std::getline(std::cin, b);
+    std::getline(std::cin, a);
+    b.append(a);
     std::cout<<"Zadaj 3. retazec: ";
-    std::getline(std::cin, c);
-    d.append(a);
-    d.append(b);
-    d.append(c);
-    std::cout<<d<<std::endl;
+    std::getline(std::cin, a);
+    b.append(a);
+    std::cout<<b<<std::endl;
     return 0;
 }
